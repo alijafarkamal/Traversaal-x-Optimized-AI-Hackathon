f63c7cda95bf0fd133b37dcfc538c04a25737ed5
alijafarkamal <ali.jafar.fast@gmail.com>
2025-04-14 18:18:29 +0500

    Remove agentpro from git tracking

--- agentpro/agentpro/__init__.py
+++
@@ -1,8 +0,0 @@
-from .agent import AgentPro
-from typing import Any
-from .tools import AresInternetTool, CodeEngine, YouTubeSearchTool, SlideGenerationTool # add more tools when available
-ares_tool = AresInternetTool()
-# code_tool = CodeEngine()
-youtube_tool = YouTubeSearchTool()
-# slide_tool = SlideGenerationTool()
-__all__ = ['AgentPro', 'ares_tool', 'youtube_tool']
